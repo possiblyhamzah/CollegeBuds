c0d9c52f19ad453481b80c87c21ba556e5a97a3a
probablyhamzah <ahmedhamzah2002@gmail.com>
2020-09-24 14:32:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-app.config["SECRET_KEY"] = 'this-really-needs-to-be-changed'
 socketio = SocketIO(app)
 
 @app.route("/")
